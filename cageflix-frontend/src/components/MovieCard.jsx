25d8e149bcec49d2e97663b00c80f72739460746
mido4315 <mohmed.mahmoud3@gmail.com>
2025-04-10 08:33:42 +0200

    feat: add MovieCard component

---
+++ cageflix-frontend/src/components/MovieCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+const MovieCard = ({ movie }) => {
+  const {
+    tconst,
+    primaryTitle,
+    startYear,
+    genres,
+    averageRating,
+    numVotes,
+    posterUrl,
+  } = movie;
+
+  return (
+    <div className="movie-card">
+      <img src={posterUrl || "./no-movie.png"} alt="No Image" />
+      <div className="mt-4">
+        <h3>{primaryTitle}</h3>
+        <div className="content">
+          <div className="rating">
+            <img src="star.svg" alt="Rating" />
+            <p>{averageRating || "N/A"}</p>
+          </div>
+
+          <p className="lang">
+            <span>•</span> EN
+          </p>
+          <p className="year">{startYear}</p>
+          <p className="genre">{genres.split(",")[0]}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MovieCard;
